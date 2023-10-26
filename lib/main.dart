4d5daad3e0306f2117a3b7e0430af94ca3a41020
honzahoff <honzahoff@gmail.com>
2023-10-26 23:36:06 +0200

    app setup - fonts, colors

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:metronome_app/screens/Main_screen.dart';
 
 void main() {
   runApp(const ProviderScope(child: MainApp()));
@@ -10,12 +11,6 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
-    );
+    return const MaterialApp(home: MainScreen());
   }
 }
---
+++ lib/resources/values/app_colors.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  //Primary
+  static const Color primary900 = Color(0xFF130928);
+  static const Color primary700 = Color(0xFF372163);
+  static const Color primary600 = Color(0xFF6C48B4);
+  static const Color primary400 = Color(0xFFA87FFB);
+  static const Color primary300 = Color(0xFFCBB1FF);
+  static const Color primary200 = Color(0xFFE5D8FF);
+
+  //Secondary
+  static const Color secondary900 = Color(0xFF10151D);
+  static const Color secondary800 = Color(0xFF10151D);
+  static const Color secondary700 = Color(0xFF161E29);
+  static const Color secondary500 = Color(0xFF2E3C51);
+  static const Color secondary300 = Color(0xFF8596B0);
+  static const Color secondary200 = Color(0xFFBFC7D2);
+
+  //Neutral
+  static const Color neutral1000 = Color(0xFF000000);
+  static const Color neutral700 = Color(0xFF191919);
+  static const Color neutral600 = Color(0xFF353535);
+  static const Color neutral400 = Color(0xFF949494);
+  static const Color neutral200 = Color(0xFFDADADA);
+  static const Color neutral0 = Color(0xFFFFFFFF);
+
+  //Black and white
+  static const Color white = neutral0;
+  static const Color black = neutral1000;
+}
