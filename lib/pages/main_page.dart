e5786d2633e60148078b04e8b762c19980129cf4
honzahoff <honzahoff@gmail.com>
2023-11-01 10:14:01 +0100

    responsivness added

--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:flutter_svg/svg.dart';
 import 'package:metronome_app/resources/values/app_colors.dart';
 import 'package:metronome_app/resources/values/app_fonts.dart';
 import 'package:metronome_app/resources/values/app_sizes.dart';
@@ -39,196 +38,208 @@ class MainPage extends ConsumerWidget {
                 iconSize: AppSizes.iconSize,
               )
             ]),
-        body: Column(children: [
-          const SizedBox(
-            height: 80,
-          ),
-          Center(
-            child: SizedBox(
-              width: 125,
-              height: 13,
-              child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Container(
-                        width: 13,
-                        height: 13,
-                        decoration: const BoxDecoration(
-                            borderRadius: BorderRadius.all(Radius.circular(10)),
-                            color: AppColors.primary300)),
-                    Container(
-                        width: 13,
-                        height: 13,
-                        decoration: const BoxDecoration(
-                            borderRadius: BorderRadius.all(Radius.circular(10)),
-                            color: AppColors.secondary500)),
-                    Container(
-                        width: 13,
-                        height: 13,
-                        decoration: const BoxDecoration(
-                            borderRadius: BorderRadius.all(Radius.circular(10)),
-                            color: AppColors.secondary500)),
-                    Container(
-                        width: 13,
-                        height: 13,
-                        decoration: const BoxDecoration(
-                            borderRadius: BorderRadius.all(Radius.circular(10)),
-                            color: AppColors.secondary500))
-                  ]),
-            ),
-          ),
-          const SizedBox(height: 80),
-          Center(
-            child: SizedBox(
-              width: 300,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  IconButton(
-                    icon: const Icon(Icons.add),
-                    color: AppColors.secondary500,
-                    iconSize: 40,
-                    onPressed: () {},
-                  ),
-                  Column(children: [
-                    Text(
-                      "Allegro",
-                      style: AppFonts.titleSmall,
-                    ),
-                    Text(
-                      "140",
-                      style: AppFonts.displayLarge,
-                    ),
-                    Text(
-                      "BPM",
-                      style: AppFonts.bodyLarge,
-                    )
-                  ]),
-                  IconButton(
-                    icon: const Icon(Icons.remove),
-                    color: AppColors.secondary500,
-                    iconSize: 40,
-                    onPressed: () {},
-                  ),
-                ],
-              ),
-            ),
-          ),
-          const SizedBox(height: 106),
-          Center(
-            child: SizedBox(
-              width: 330,
-              child: SliderTheme(
-                  data: const SliderThemeData(
-                      thumbColor: AppColors.primary400,
-                      activeTrackColor: AppColors.primary400,
-                      inactiveTrackColor: AppColors.secondary700),
-                  child: Slider(value: 0.2, onChanged: (value) {})),
-            ),
-          ),
-          const SizedBox(height: 105),
-          Center(
-            child: GestureDetector(
-              child: Container(
-                width: 160,
-                height: 50,
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(80),
-                    border: Border.all(color: AppColors.primary400)),
-                child: Center(
-                    child: Text(
-                  "Tap me",
-                  style: AppFonts.bodyLarge,
-                )),
-              ),
-            ),
-          ),
-          Center(
-            child: Stack(children: [
+        body: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Spacer(flex: 1),
               Center(
-                child: Container(
-                  width: 90,
-                  height: 90,
-                  margin: const EdgeInsets.only(top: 40),
-                  child: Container(
-                    decoration: const BoxDecoration(
-                        color: AppColors.primary400,
-                        borderRadius: BorderRadius.all(Radius.circular(50))),
-                    child: const Icon(
-                      Icons.play_arrow,
-                      size: 40,
-                    ),
-                  ),
-                ),
-              ),
-              Center(
-                child: Container(
-                  width: 320,
-                  height: 90,
-                  margin: const EdgeInsets.only(top: 48),
+                child: SizedBox(
+                  width: 125,
+                  height: 13,
                   child: Row(
                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
-                        Column(
-                          children: [
-                            Container(
-                              width: 65,
-                              height: 65,
-                              decoration: BoxDecoration(
-                                  color: AppColors.secondary900,
-                                  borderRadius: const BorderRadius.all(
-                                      Radius.circular(50)),
-                                  border: Border.all(
-                                      color: AppColors.secondary500)),
-                              child: const Icon(
-                                Icons.music_note,
-                                color: AppColors.secondary200,
-                              ),
-                            ),
-                            Container(
-                              margin: const EdgeInsets.only(top: 5),
-                              child: const Text(
-                                "Sound",
-                                style: TextStyle(color: AppColors.secondary200),
-                              ),
-                            )
-                          ],
-                        ),
-                        Column(
-                          children: [
-                            Container(
-                              width: 65,
-                              height: 65,
-                              decoration: BoxDecoration(
-                                  color: AppColors.secondary900,
-                                  borderRadius: const BorderRadius.all(
-                                      Radius.circular(50)),
-                                  border: Border.all(
-                                      color: AppColors.secondary500)),
-                              child: const Center(
-                                child: Text(
-                                  "4/4",
-                                  textAlign: TextAlign.center,
-                                  style:
-                                      TextStyle(color: AppColors.secondary200),
-                                ),
-                              ),
-                            ),
-                            Container(
-                              margin: const EdgeInsets.only(top: 5),
-                              child: const Text(
-                                "Signature",
-                                style: TextStyle(color: AppColors.secondary200),
-                              ),
-                            )
-                          ],
-                        )
+                        Container(
+                            width: 13,
+                            height: 13,
+                            decoration: const BoxDecoration(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(10)),
+                                color: AppColors.primary300)),
+                        Container(
+                            width: 13,
+                            height: 13,
+                            decoration: const BoxDecoration(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(10)),
+                                color: AppColors.secondary500)),
+                        Container(
+                            width: 13,
+                            height: 13,
+                            decoration: const BoxDecoration(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(10)),
+                                color: AppColors.secondary500)),
+                        Container(
+                            width: 13,
+                            height: 13,
+                            decoration: const BoxDecoration(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(10)),
+                                color: AppColors.secondary500))
                       ]),
                 ),
               ),
+              const SizedBox(height: 80),
+              //Spacer(flex: 1),
+              Center(
+                child: SizedBox(
+                  width: 300,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      IconButton(
+                        icon: const Icon(Icons.add),
+                        color: AppColors.secondary500,
+                        iconSize: 40,
+                        onPressed: () {},
+                      ),
+                      Column(children: [
+                        Text(
+                          "Allegro",
+                          style: AppFonts.titleSmall,
+                        ),
+                        Text(
+                          "140",
+                          style: AppFonts.displayLarge,
+                        ),
+                        Text(
+                          "BPM",
+                          style: AppFonts.bodyLarge,
+                        )
+                      ]),
+                      IconButton(
+                        icon: const Icon(Icons.remove),
+                        color: AppColors.secondary500,
+                        iconSize: 40,
+                        onPressed: () {},
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              const Spacer(flex: 2),
+              Center(
+                child: SizedBox(
+                  width: 330,
+                  child: SliderTheme(
+                      data: const SliderThemeData(
+                          thumbColor: AppColors.primary400,
+                          activeTrackColor: AppColors.primary400,
+                          inactiveTrackColor: AppColors.secondary700),
+                      child: Slider(value: 0.2, onChanged: (value) {})),
+                ),
+              ),
+              //const SizedBox(height: 105),
+              const Spacer(flex: 2),
+              Center(
+                child: GestureDetector(
+                  child: Container(
+                    width: 160,
+                    height: 50,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(80),
+                        border: Border.all(color: AppColors.primary400)),
+                    child: Center(
+                        child: Text(
+                      "Tap me",
+                      style: AppFonts.bodyLarge,
+                    )),
+                  ),
+                ),
+              ),
+              Center(
+                child: Stack(children: [
+                  Center(
+                    child: Container(
+                      width: 90,
+                      height: 90,
+                      margin: const EdgeInsets.only(top: 40),
+                      child: Container(
+                        decoration: const BoxDecoration(
+                            color: AppColors.primary400,
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(50))),
+                        child: const Icon(
+                          Icons.play_arrow,
+                          size: 40,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Center(
+                    child: Container(
+                      width: 320,
+                      height: 90,
+                      margin: const EdgeInsets.only(top: 48),
+                      child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Column(
+                              children: [
+                                Container(
+                                  width: 65,
+                                  height: 65,
+                                  decoration: BoxDecoration(
+                                      color: AppColors.secondary900,
+                                      borderRadius: const BorderRadius.all(
+                                          Radius.circular(50)),
+                                      border: Border.all(
+                                          color: AppColors.secondary500)),
+                                  child: const Icon(
+                                    Icons.music_note,
+                                    color: AppColors.secondary200,
+                                  ),
+                                ),
+                                Container(
+                                  margin: const EdgeInsets.only(top: 5),
+                                  child: const Text(
+                                    "Sound",
+                                    style: TextStyle(
+                                        color: AppColors.secondary200),
+                                  ),
+                                )
+                              ],
+                            ),
+                            Column(
+                              children: [
+                                Container(
+                                  width: 65,
+                                  height: 65,
+                                  decoration: BoxDecoration(
+                                      color: AppColors.secondary900,
+                                      borderRadius: const BorderRadius.all(
+                                          Radius.circular(50)),
+                                      border: Border.all(
+                                          color: AppColors.secondary500)),
+                                  child: const Center(
+                                    child: Text(
+                                      "4/4",
+                                      textAlign: TextAlign.center,
+                                      style: TextStyle(
+                                          color: AppColors.secondary200),
+                                    ),
+                                  ),
+                                ),
+                                Container(
+                                  margin: const EdgeInsets.only(top: 5),
+                                  child: const Text(
+                                    "Signature",
+                                    style: TextStyle(
+                                        color: AppColors.secondary200),
+                                  ),
+                                )
+                              ],
+                            )
+                          ]),
+                    ),
+                  ),
+                ]),
+              ),
+              const SizedBox(
+                height: 20,
+              )
             ]),
-          )
-        ]),
       ),
     );
   }
